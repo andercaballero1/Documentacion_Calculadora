0ca45cc6e8608d72528131686b26f55a285bae60
irakaslea <irakaslea@DESKTOP-Q2BRC07>
2022-04-06 10:30:41 +0200

    Ejercicio Javadoc, logs, Formatter, Filter - Calculadora

--- Calculadora/src/logs/FormatoHTML2.java
+++ Calculadora/src/logs/FormatoHTML2.java
@@ -10,17 +10,35 @@ public class FormatoHTML2 extends Formatter {
 	public String format(LogRecord record) {
 		String[] partes = record.getMessage().split(" ");
 		
-		return "<TR><TD> " + partes[1] + " </TD><TD> " + partes[3] + " </TD><TD> " + partes[5] + " </TD><TD> " + partes[7] + " </TD></TR>";
+		return "<TR> \n "
+					+ "<TD> " + partes[1] + "</TD>"
+					+ "<TD> " + partes[3] + "</TD>"
+					+ "<TD> " + partes[5] + "</TD>"
+					+ "<TD> " + partes[7] + "</TD> \n "
+				+ "</TR> \n";
 	}
 	
 	@Override
 	public String getHead(Handler h) {
-        return "<HTML> \n <HEAD> <link rel='stylesheet' href='C:\\Users\\irakaslea\\git\\Calculadora\\Calculadora\\src\\style.css'></HEAD> \n <BODY><TABLE><TBODY><TR><TH>Operacion</TH><TH>Operando1</TH><TH>Operando2</TH><TH>Resultado</TH>";
+        return "<HTML> \n"
+        		+ "<HEAD> <link rel='stylesheet' href='C:\\Users\\irakaslea\\git\\Calculadora\\Calculadora\\src\\style.css'></HEAD> \n "
+        		+ "<BODY>\n"
+        		+ "<TABLE>\n"
+        		+ 	"<TBODY>\n"
+        		+ 		"<TR>\n"
+        		+ 			"<TH>Operacion</TH>"
+        		+ 			"<TH>Operando1</TH>"
+        		+ 			"<TH>Operando2</TH>"
+        		+ 			"<TH>Resultado</TH> \n";
     }
 
 	@Override
     public String getTail(Handler h) {
-        return "</TR></TBODY></TABLE> </BODY> \n </HTML> \n";
+        return 			"</TR> \n"
+        		+ 	"</TBODY> \n"
+        		+  "</TABLE> \n"
+        		+  "</BODY> \n"
+        		+ "</HTML>";
     }
 
 }
